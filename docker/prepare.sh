c8e0d9b897ad1e702ce2a488403e1d22e86ec72f
lonelyandrew <lonelyandrew@outlook.com>
2019-07-10 09:28:07 +0800

    update

---
+++ docker/prepare.sh
@@ -0,0 +1,10 @@
+wget https://codeload.github.com/VundleVim/Vundle.vim/zip/master -O vundle.zip
+wget https://codeload.github.com/airblade/vim-gitgutter/zip/master -O vim-gitgutter.zip
+wget https://codeload.github.com/majutsushi/tagbar/zip/master -O tagbar.zip
+wget https://codeload.github.com/wincent/command-t/zip/master -O command-t.zip
+wget https://codeload.github.com/tpope/vim-commentary/zip/master -O vim-commentary.zip
+wget https://codeload.github.com/nvie/vim-flake8/zip/master -O vim-flake8.zip
+wget https://codeload.github.com/tpope/vim-surround/zip/master -O vim-surround.zip
+wget https://codeload.github.com/davidhalter/jedi-vim/zip/master -O jedi-vim.zip
+wget https://codeload.github.com/ycm-core/YouCompleteMe/zip/master -O ycm.zip
+
